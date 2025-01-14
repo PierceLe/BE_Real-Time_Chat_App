7efe6b438c4ae387ed70bdf3926693d78493ca83
PierceLe <haichau.hvsg@gmail.com>
2025-01-15 05:59:31 +0700

    feat: finish basic chatapp for practising STOMP websocket in springboot without authentication and authorization

--- backend/main/java/com/piercele/chat/config/WebSocketConfig.java
+++ backend/main/java/com/piercele/chat/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")  // Allow all origins for WebSocket
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
